c4da129018d0e8ea553e8e3e9027f9be09c3ff0f
iced-code github <ayaanmodak1@gmail.com>
2026-02-24 23:04:19 -0500

    Table testing

--- scripts.js
+++ scripts.js
@@ -7,3 +7,42 @@ function updateCounter(){
     mainDiv.style.backgroundColor = `rgb(${211-(10*count)}, ${211-(5*count)}, 211)`;
     counter.innerText = count;
 }
+
+
+function createTableFromJson() {
+  // Get the container element
+  const container = document.getElementById('mainDiv');
+
+  // Create the main table element
+  const table = document.createElement('table');
+  
+  // Get the keys from the first object to use as table headers
+  const columns = ["ID", "Challenge_Name","Category","Difficulty","Points","Attempts","Attempts_Successful","Attempts_Fail", "Success_Rate"]
+
+  let id = 1;
+  // Create table body rows
+  columns.forEach(item => {
+    const dataRow = document.createElement('tr');
+    columns.forEach(col => {
+      const td = document.createElement('td');
+      td.innerHTML = "X";
+
+      td.onclick = function () {
+        if(td.style.color === "transparent"){
+          td.style.color = "initial";
+        }
+        else {
+          td.style.color = "transparent";
+        }
+      };
+
+      dataRow.appendChild(td);
+    });
+    table.appendChild(dataRow);
+  });
+
+  // Append the created table to the container
+  container.appendChild(table);
+}
+
+createTableFromJson();
